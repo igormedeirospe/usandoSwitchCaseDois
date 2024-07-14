527fa84b22f817f9c4ccdc5f90df95950d42e7d4
Igor <igormedeiros407@gmail.com>
2024-07-14 00:42:27 -0300

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,30 @@
+import javax.swing.*;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.printf("Bem vindo");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    int mes;
+    mes = Integer.parseInt(JOptionPane.showInputDialog("Digite o mes de 1 a 12"));
+
+    switch (mes){
+
+    case 1: case 2: case 3: case 4: case 5:
+    System.out.println("O mes esta entre 1 e 5");
+    break;
+    case 6: case 7: case 8: case 9: case 10:
+        System.out.println("O mes esta entre 6 e 10");
+        break;
+        case 11: case 12:
+            System.out.println("O mes esta entre 11 e 12");
+            break;
+        default: System.out.println("O mes esta invalido");
+    }
+
+
+
+
         }
     }
-}
