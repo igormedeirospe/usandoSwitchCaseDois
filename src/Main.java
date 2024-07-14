fba5c702d8fea4500bf69a99d040d282f40678a0
Igor <igormedeiros407@gmail.com>
2024-07-14 00:43:13 -0300

    commit

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import javax.swing.*;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Bem vindo");
+
 
     int mes;
     mes = Integer.parseInt(JOptionPane.showInputDialog("Digite o mes de 1 a 12"));
